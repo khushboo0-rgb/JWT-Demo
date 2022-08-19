2d199f5ff089188e156aedf7aa310eb29e4c1e26
unknown <cogjava3119@iiht.tech>
2022-08-18 23:15:15 -0700

    adding security

--- src/main/java/com/token/demo/JwtDemoApplication.java
+++ src/main/java/com/token/demo/JwtDemoApplication.java
@@ -2,6 +2,9 @@ package com.token.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class JwtDemoApplication {
@@ -9,5 +12,10 @@ public class JwtDemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(JwtDemoApplication.class, args);
 	}
-
+  
+	@Bean
+	public PasswordEncoder passwordEncoder()
+	{
+		return NoOpPasswordEncoder.getInstance();
+	}
 }
